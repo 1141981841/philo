eed6862dfce42cd30ad91846e87ce7e91683e7c3
qq365043710 <365043710@qq.com>
2015-11-14 22:04:40 +0800

    philo1

---
+++ book/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Author(models.Model):
+	name = models.CharField(max_length=120)
+	authorid = models.IntegerField()
+	age = models.IntegerField()
+	country = models.CharField(max_length=20)
+
+class Book(models.Model):
+	title = models.CharField(max_length=50)
+	isbn = models.CharField(max_length=50)
+	authorid = models.ForeignKey(Author)
+	publisher = models.CharField(max_length=50)
+	publication_date = models.CharField(max_length=20)
+	price = models.IntegerField()
+
+
+
+
+
+
+
+
+
+
+
+
